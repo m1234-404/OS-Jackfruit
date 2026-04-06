bc07cf130795fbbb7e5f5de09be53395344669f6
m1234-404 <medhaop25@gmail.com>
2026-04-06 15:27:58 +0530

    Update monitor_ioctl.h

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -1,20 +1,30 @@
 #ifndef MONITOR_IOCTL_H
 #define MONITOR_IOCTL_H
-#include <linux/ioctl.h>
-#include <sys/types.h>
 
+#include <linux/ioctl.h>
+
+// This part hides the user-space headers from the kernel
+#ifndef __KERNEL__
+#include <sys/types.h>
+#endif
+
+// Define types that work for both kernel and user-space
 typedef struct { char id[32], rootfs[256], command[256]; } child_config_t;
 typedef enum { CMD_RUN = 2, CMD_PS = 3, CMD_STOP = 5 } command_kind_t;
+
 typedef struct {
     command_kind_t kind;
     char container_id[32], rootfs[256], command[256];
     unsigned long soft_limit_bytes, hard_limit_bytes;
 } control_request_t;
+
 struct monitor_request {
-    pid_t pid;
+    int pid; // Changed pid_t to int for better kernel/user compatibility
     unsigned long soft_limit_bytes, hard_limit_bytes;
     char container_id[32];
 };
+
 #define MONITOR_MAGIC 'm'
 #define MONITOR_REGISTER _IOW(MONITOR_MAGIC, 1, struct monitor_request)
+
 #endif
